5efea281e483bee30cb9874ceb8967131cc89fb8
xseko60 tr60 <158826606+WormateSerkanConnect@users.noreply.github.com>
2025-04-28 23:41:31 +0300

    Update gamewfc.js

--- js/gamewfc.js
+++ js/gamewfc.js
@@ -1,7 +1,6 @@
 
 var SITE_XTHOST = "https://wormateserkanconnect.github.io/DV";
-var linkCSS = "https://wormateserkanconnect.github.io/DV/css/game.css?v=" + TIME;
-// دالة لتحميل الأنماط
+
 window.detectLog = null;
 const _wrmxt = {
     BETAisSkinCustom(input) {
