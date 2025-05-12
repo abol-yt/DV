c7e77c8d3d78a46157316e22815fda1fece06dee
xseko60 tr60 <158826606+WormateSerkanConnect@users.noreply.github.com>
2025-05-12 19:28:02 +0300

    Update users.php

--- api/users.php
+++ api/users.php
@@ -33,11 +33,11 @@
         },
         {
             "id": 4,
-            "cliente_NOMBRE": "yosef",
-            "cliente_ID": "gg_102224856542212549142",
+            "cliente_NOMBRE": "Ali ",
+            "cliente_ID": "gg_118125883995246372181",
             "Client_KeyAccecs": "XTPRIVATESKIN",
             "cliente_DateStarted": "04-02-2025",
-            "cliente_DateExpired": "16-02-2026",
+            "cliente_DateExpired": "16-02-2029",
             "status": 1,
             "Client_VisibleSkin": "1023"
         },
